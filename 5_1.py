72efbee39d5e166208128a3fa4e2cefc482125d1
MIxtara <mixtaralal2@mail.ru>
2023-04-17 18:44:05 +0300

    5_1 done

---
+++ 5_1.py
@@ -0,0 +1,9 @@
+n = int(input("Введите количество"))
+m = int(input('Введите кратность'))
+k = int(input('Больше этого числа'))
+while n:
+    if not k % m:
+        print(k)
+        n-=1
+    k+=1
+
