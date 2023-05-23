36a3072a3701adfc2fa584f5246ec44bdd6a2f03
MIxtara <mixtaralal2@mail.ru>
2023-05-23 15:32:37 +0300

    prof1 done

--- 5_1.py
+++ 5_1.py
@@ -1,9 +1,11 @@
-n = int(input("Введите количество"))
-m = int(input('Введите кратность'))
-k = int(input('Больше этого числа'))
-while n:
-    if not k % m:
-        print(k)
-        n-=1
-    k+=1
+# n = int(input("Введите количество"))
+# m = int(input('Введите кратность'))
+# k = int(input('Больше этого числа'))
+# while n:
+#     if not k % m:
+#         print(k)
+#         n-=1
+#     k+=1
+#
 
+print(4 % 2 )
